c9990b6b83b6b282009be445e722d4d59e3d1f1c
Bima Ardhia <bimaardhia@gmail.com>
2023-08-03 01:51:45 +0700

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+    
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+
+        <div className=' max-w-screen-lg p-4 mx-auto flex-col justify-center w-full h-full'>
+
+            <div className=' pb-8'>
+                <p className=' text-4xl font-bold inline border-b-4 border-gray-500  '>
+                    About
+                    </p>
+            </div>
+            <p className=' text-xl mt-20'>
+            Saya adalah seorang siswa Rekayasa Perangkat Lunak tahun kedua di SMKN 1 Surabaya dengan semangat yang tinggi untuk mengembangkan diri dan menambah portofolio saya. Dalam setahun terakhir, saya telah memperoleh banyak pelajaran berharga yang telah meningkatkan kemampuan saya. Sebagai contoh, saya telah berhasil menciptakan berbagai poster produk yang menarik konsumen serta menguasai bahasa pemrograman C++, Java, dan Python hingga pada tingkat yang tinggi.
+            </p>
+
+            <br />
+
+            <p className=' text-xl'>
+            Selain itu, saya juga telah mendalami bidang Internet of Things (IoT) dan berhasil menciptakan dua produk inovatif, yaitu sistem parkir dan plant monitoring, yang berfungsi secara efektif dalam kehidupan sehari-hari manusia. Selama perjalanan pendidikan saya, saya juga berhasil menciptakan model 3D dari karakter Doraemon dan makanan ringan Oreo yang menarik dan menggugah minat.
+            </p>
+
+            <br />
+
+            <p className=' text-xl'>
+            Saya dikenal sebagai seorang individu yang serius dalam proses belajar, dan komitmen saya terhadap peningkatan diri terus mendorong saya untuk mencapai hasil yang lebih baik. Saya selalu mengutamakan kualitas dalam setiap proyek yang saya kerjakan, dengan fokus pada inovasi dan nilai jual yang optimal. Saya percaya bahwa dedikasi dan ketekunan saya dalam belajar akan terus membawa kesuksesan di masa depan, dan saya sangat antusias untuk terus mengembangkan kemampuan saya dalam dunia rekayasa perangkat lunak.
+
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default About
